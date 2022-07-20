29f366a1f09b43230187c5439e17d2a3ae270434
darkikim <maxime.goethals.59@gmail.com>
2022-07-20 14:11:07 +0200

    Configuration Git/Docker

---
+++ microservice_commande/src/main/java/poei/orsys2/commande/CommandeApplication.java
@@ -0,0 +1,13 @@
+package poei.orsys2.commande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandeApplication.class, args);
+	}
+
+}
---
+++ microservice_commande/src/test/java/poei/orsys2/commande/CommandeApplicationTests.java
@@ -0,0 +1,13 @@
+package poei.orsys2.commande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
