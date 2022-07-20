a5c73aef27c057732334acb313c06535710bae90
Dorian <dorianguerry@gmail.com>
2022-07-20 15:09:05 +0200

    Produit repository basique fini

--- microservice_produit/src/main/java/poei/orsys2/produit/dao/ProduitRepository.java
+++ microservice_produit/src/main/java/poei/orsys2/produit/dao/ProduitRepository.java
@@ -1,5 +1,5 @@
 package poei.orsys2.produit.dao;
-
+import poei.orsys2.produit.model.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProduitRepository extends JpaRepository<Produit,Long> {
