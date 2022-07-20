76a306c9714fae5735ffde5eb51402d68e4e4dd8
darkikim <maxime.goethals.59@gmail.com>
2022-07-20 13:56:42 +0200

    Configuration Git/Docker

---
+++ microservice_produit/src/main/java/poei/orsys2/produit/ProduitApplication.java
@@ -0,0 +1,13 @@
+package poei.orsys2.produit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitApplication.class, args);
+	}
+
+}
---
+++ microservice_produit/src/test/java/poei/orsys2/produit/ProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package poei.orsys2.produit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
