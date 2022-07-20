be28ebc536af60ae3b5fd6210f492c825f595974
Dorian <dorianguerry@gmail.com>
2022-07-20 16:52:08 +0200

    ProduitRepository avec correction du type

--- microservice_produit/src/main/java/poei/orsys2/produit/dao/ProduitRepository.java
+++ microservice_produit/src/main/java/poei/orsys2/produit/dao/ProduitRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProduitRepository extends JpaRepository<Produit,Integer> {
-    Page<Produit> findByTitreContaining(String titre);
+
     List<Produit> searchProduct(String search);
 }
 
