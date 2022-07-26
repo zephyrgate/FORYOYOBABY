ed37e17e75755c29d4f163ee56397f1b5d61ef85
Dorian <dorianguerry@gmail.com>
2022-07-26 09:20:00 +0200

    add annotation CrossOrigin(origins = "http://localhost:4200") in the ProduitRestController

--- microservice_produit/src/main/java/poei/orsys2/produit/web/ProduitRestController.java
+++ microservice_produit/src/main/java/poei/orsys2/produit/web/ProduitRestController.java
@@ -1,6 +1,7 @@
 package poei.orsys2.produit.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProduitRestController {
     @Autowired
     private final ProduitRepository produitRepository;
